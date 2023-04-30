0f2a01b5d0f44102b0669e9e9c9d490af0050ec4
ivanjermakov <ivanjermakov1@gmail.com>
2023-05-01 00:27:47 +0200

    Fixed background color

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,2 +1,13 @@
+require("catppuccin").setup({
+    flavour = "mocha",
+    color_overrides = {
+        all = {
+            base = "#111111",
+            mantle = "#111111",
+            crust = "#111111",
+        }
+    }
+})
+
 vim.cmd.colorscheme "catppuccin"
 

ec7d5bf96fbde845e827ad9756d55a6e4a20c732
ivanjermakov <ivanjermakov1@gmail.com>
2023-08-09 16:38:50 +0200

    Autosave in vcs dirs

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -5,3 +5,13 @@ vim.api.nvim_create_autocmd("FileType", {
         -- vim.opt.cindent = true
     end
 })
+
+vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+    pattern = { "*" },
+    callback = function()
+        if #vim.api.nvim_buf_get_option(0, "buftype") == 0
+            and #vim.api.nvim_get_runtime_file(".git", false) > 0 then
+            vim.api.nvim_cmd({ cmd = "write" }, {})
+        end
+    end
+})

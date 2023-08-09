0cfe5825d6dc9ccb768ee4afc54e2c466bebb034
ivanjermakov <ivanjermakov1@gmail.com>
2023-08-10 00:50:00 +0200

    Migrate to lazy.nvim

---
+++ lua/config/lazy.lua
@@ -0,0 +1,155 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {
+        "ivanjermakov/mellow.nvim",
+        config = function()
+            vim.cmd [[colorscheme mellow]]
+        end
+    },
+    {
+        "ivanjermakov/troublesum.nvim",
+        config = function()
+            require("troublesum").setup()
+        end
+    },
+    "ivanjermakov/telescope-file-structure.nvim",
+
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.0",
+        dependencies = { { "nvim-lua/plenary.nvim" } }
+    },
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust" },
+            sync_install = false,
+            indent = { enable = true },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<c-w>",
+                    node_incremental = "<c-w>",
+                    scope_incremental = ".",
+                    node_decremental = ",",
+                },
+            },
+
+        },
+    },
+    -- "nvim-treesitter/playground",
+
+    "nois-lang/nois.vim",
+
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = 'v2.x',
+        dependencies = {
+            { "neovim/nvim-lspconfig" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
+
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lua" },
+
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
+        }
+    },
+
+    {
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup()
+        end
+    },
+
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+        end
+    },
+
+    {
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup(nil)
+        end
+    },
+
+    "shortcuts/no-neck-pain.nvim",
+
+    "mfussenegger/nvim-dap",
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "mfussenegger/nvim-dap" }
+    },
+    {
+        "mxsdev/nvim-dap-vscode-js",
+        dependencies = { "mfussenegger/nvim-dap" }
+    },
+    {
+        "microsoft/vscode-js-debug",
+        build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"
+    },
+
+    {
+        "kylechui/nvim-surround",
+        config = function()
+            require("nvim-surround").setup()
+        end
+    },
+
+    "folke/neodev.nvim",
+
+    "lukas-reineke/indent-blankline.nvim",
+
+    {
+        "folke/flash.nvim",
+        config = function()
+            require("flash").setup({
+                label = {
+                    uppercase = false
+                },
+                modes = {
+                    char = {
+                        jump_labels = true
+                    }
+                }
+            })
+        end
+    },
+})
+
+require("no-neck-pain").setup({
+    width = 140,
+    autocmds = {
+        enableOnVimEnter = true,
+        enableOnTabEnter = true
+    }
+})

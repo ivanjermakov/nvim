0cb99d72d48d6fe0ac71096b0d38b57e47cf47e9
ivanjermakov <ivanjermakov1@gmail.com>
2023-08-10 02:28:24 +0200

    Fix autosave error in unnamed buffer

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -10,7 +10,8 @@ vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
     pattern = { "*" },
     callback = function()
         if #vim.api.nvim_buf_get_option(0, "buftype") == 0
-            and #vim.api.nvim_get_runtime_file(".git", false) > 0 then
+            and #vim.api.nvim_get_runtime_file(".git", false) > 0
+            and #vim.api.nvim_buf_get_name(0) > 0 then
             vim.api.nvim_cmd({ cmd = "write" }, {})
         end
     end

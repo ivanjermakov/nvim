35a1fb9416908e2d13fe5fc714f0c436af83364d
ivanjermakov <ivanjermakov1@gmail.com>
2023-11-07 19:19:24 +0100

    Fix autosave to trigger lsp

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -7,8 +7,9 @@ vim.api.nvim_create_autocmd("FileType", {
 
 vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
     pattern = { "*" },
+    nested = true,
     callback = function()
-        if #vim.api.nvim_buf_get_option(0, "buftype") == 0
+        if #vim.api.nvim_get_option_value("buftype", {}) == 0
             and #vim.api.nvim_get_runtime_file(".git", false) > 0
             and #vim.api.nvim_buf_get_name(0) > 0 then
             vim.api.nvim_cmd({ cmd = "write", mods = { silent = true } }, {})

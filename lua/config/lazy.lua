7c88a5ebd235506b6d88f7d839d1f64878fb65f1
ivanjermakov <ivanjermakov1@gmail.com>
2023-12-30 01:00:30 +0100

    Tag names highlight

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,6 +13,8 @@ require("lazy").setup({
         config = function()
             vim.cmd [[colorscheme nellow]]
             vim.api.nvim_set_hl(0, "TelescopeBorder", { link = "FloatBorder" })
+            vim.api.nvim_set_hl(0, "jsxIntrinsicTagName", { link = "Special" })
+            vim.api.nvim_set_hl(0, "htmlTagName", { link = "Special" })
         end
     },
     {

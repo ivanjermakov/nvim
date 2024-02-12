2f840e0d59d1c6aa94034b821b22b5f1f09fa99d
ivanjermakov <ivanjermakov1@gmail.com>
2024-02-12 19:18:20 +0100

    Fix cursor color

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -19,6 +19,7 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
+vim.opt.guicursor = "n-v-c-sm:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor"
 
 vim.opt.scrolloff = 8
 

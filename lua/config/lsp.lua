2ba5f3fd3eeb59e9244f816bd01abcba9ea629fa
ivanjermakov <ivanjermakov1@gmail.com>
2025-06-12 11:28:51 +0200

    Another try with `blink.cmp`

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -133,7 +133,7 @@ local servers = {
     zls = {},
 }
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("blink.cmp").get_lsp_capabilities()
 capabilities.textDocument.semanticTokens = nil
 capabilities.textDocument.completion.completionItem.snippetSupport = false
 for name, server in pairs(servers) do
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,32 +1,35 @@
 return {
-    { "neovim/nvim-lspconfig", },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/cmp-nvim-lua" },
-    { "hrsh7th/cmp-buffer" },
-    { "hrsh7th/cmp-path" },
+    { "neovim/nvim-lspconfig" },
     {
-        "hrsh7th/nvim-cmp",
-        config = function()
-            local cmp = require('cmp')
-
-            cmp.setup {
-                completion = {
-                    completeopt = 'menu,menuone,noinsert',
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<cr>"] = cmp.mapping.confirm({ select = true }),
-                    ["<m-cr>"] = cmp.mapping.complete({ reason = cmp.ContextReason.Auto }),
-                }),
-                sources = {
-                    { name = 'nvim_lsp' },
-                    { name = 'path' },
-                },
+        "saghen/blink.cmp",
+        event = "VimEnter",
+        branch = "main",
+        opts = {
+            keymap = {
+                preset = "none",
+                ["<up>"] = { "select_prev", "fallback" },
+                ["<down>"] = { "select_next", "fallback" },
+                ["<cr>"] = { "accept", "fallback" },
+            },
+            sources = {
+                default = { "lsp", "path", "buffer" },
+            },
+            menu = { auto_show = true },
+            completion = {
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 0
+                }
+            },
+            signature = { enabled = true },
+            fuzzy = {
+                implementation = "lua"
             }
-        end
+        }
     },
     {
         "nvimtools/none-ls.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = { "nvim-lua/plenary.nvim", "nvimtools/none-ls-extras.nvim", },
         config = function()
             local null_ls = require("null-ls")
             null_ls.setup({

0dc0b2376b6e30df47751e2ab1983173e491271e
ivanjermakov <ivanjermakov1@gmail.com>
2026-08-19 23:20:04 +0200

    Colorscheme

--- lua/config/hi.lua
+++ lua/config/hi.lua
@@ -1,4 +1,11 @@
 vim.cmd "colorscheme lunaperche"
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+
+for _, hi in pairs({ "Normal", "NormalFloat", "FloatBorder", "Special", "Identifier", "Type" }) do
+    vim.api.nvim_set_hl(0, hi, { bg = "none" })
+end
+
+vim.api.nvim_set_hl(0, "PreProc", { link = "Normal" })
+
+vim.api.nvim_set_hl(0, "Constant", { fg = "#f0c5a9" })
+vim.api.nvim_set_hl(0, "String", { fg = "#a6d189" })
+vim.api.nvim_set_hl(0, "Comment", { fg = "#9998a8" })

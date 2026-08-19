bad5f1224c28867c4d0a19cd7f14f3b1fff9aa09
ivanjermakov <ivanjermakov1@gmail.com>
2026-08-20 01:12:56 +0200

    Colorscheme

--- lua/config/hi.lua
+++ lua/config/hi.lua
@@ -5,6 +5,12 @@ for _, hi in pairs({ "NormalFloat", "FloatBorder", "Special", "Identifier", "Typ
     vim.api.nvim_set_hl(0, hi, { link = "Normal" })
 end
 
+vim.api.nvim_set_hl(0, "Visual", { bg = "#3e3e43" })
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "#1b1b1d" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#57575f" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#57575f" })
+vim.api.nvim_set_hl(0, "MatchParen", { fg = "#f0c5a9" })
+
 vim.api.nvim_set_hl(0, "PreProc", { link = "Normal" })
 
 vim.api.nvim_set_hl(0, "Constant", { fg = "#f0c5a9" })

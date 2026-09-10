e979d15caa814dcdf7c9bc607795a36d8f30e0f8
ivanjermakov <ivanjermakov1@gmail.com>
2026-09-10 02:33:08 +0200

    Sort buffer picker

--- lua/config/mini.lua
+++ lua/config/mini.lua
@@ -1,6 +1,24 @@
 vim.pack.add({ "https://github.com/nvim-mini/mini.pick" })
 local mini_pick = require("mini.pick")
 
+mini_pick.registry.buffers_recent = function()
+    local items, cwd = {}, vim.fn.getcwd()
+    for _, buf_info in ipairs(vim.fn.getbufinfo()) do
+        if buf_info.listed == 1 then
+            local name = vim.fs.relpath(cwd, buf_info.name) or buf_info.name
+            table.insert(items, { text = name, bufnr = buf_info.bufnr, _lastused = buf_info.lastused })
+        end
+    end
+
+    table.sort(items, function(a, b) return a._lastused > b._lastused end)
+
+    local show = function(buf_id, items_to_show, query)
+        mini_pick.default_show(buf_id, items_to_show, query, { show_icons = true })
+    end
+    local opts = { source = { name = 'Buffers', items = items, show = show } }
+    return mini_pick.start(opts)
+end
+
 vim.pack.add({ "https://github.com/nvim-mini/mini.extra" })
 local mini_extra = require("mini.extra")
 mini_extra.setup()
@@ -14,7 +32,7 @@ mini_pick.setup({
     }
 })
 vim.keymap.set({ "n" }, "<c-n>", function() mini_pick.builtin.files() end)
-vim.keymap.set({ "n" }, "<c-e>", function() mini_pick.builtin.buffers() end)
+vim.keymap.set({ "n" }, "<c-e>", function() mini_pick.registry.buffers_recent() end)
 vim.keymap.set({ "n" }, "<c-f>", function() mini_pick.builtin.grep_live() end)
 vim.keymap.set({ "n" }, "<leader>d", function()
     vim.lsp.buf.definition({
